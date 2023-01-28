50a951d6e2dcb075f4bc5eb26420e419d9c6c6eb
ARMA69 <OleksandrRizun@gmail.com>
2023-01-28 22:09:11 +0100

    mapUsers pg

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+module.exports.mapUsers = (userArray) => {
+  return userArray
+    .map(
+      (u) =>
+        `('${u.firstName}', '${u.lastName}', '${u.email}', ${u.isSubscribe})`
+    )
+    .join(",");
+};
