7e90c143f8fe5a02289633ab518e7a3784144a55
ARMA69 <OleksandrRizun@gmail.com>
2023-02-07 20:21:43 +0100

    EXISTS

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,3 +1,55 @@
+    07.02.2023
+
+    ---EXISTS
+
+ ---IN, NOT IN, SOME, ANY, EXISTS, ALL
+
+SELECT * FROM users AS u
+WHERE u.id IN(
+  SELECT customer_id FROM orders
+);
+
+SELECT * FROM products AS p
+WHERE p.id NOT IN(
+  SELECT product_id FROM orders_to_products
+);
+
+SELECT EXISTS(
+SELECT * FROM users
+WHERE id = 1166);
+
+SELECT * FROM users AS u
+WHERE EXISTS(
+  SELECT * FROM orders AS o
+  WHERE u.id = o.customer_id
+);
+
+
+SELECT * FROM users AS u
+WHERE u.id = ANY(
+  SELECT customer_id FROM orders
+);
+
+SELECT * FROM products AS p
+WHERE p.id != ALL (
+  SELECT product_id FROM orders_to_products
+);
+
+SELECT * FROM products AS p
+WHERE p.id = ANY(
+  SELECT product_id FROM orders_to_products AS otp
+  WHERE order_id = SOME(
+    SELECT id FROM orders AS o 
+    WHERE o.customer_id = 1255
+  )
+);
+
+SELECT * FROM products AS p
+JOIN orders_to_products AS otp
+ON otp.product_id = p.id
+JOIN orders AS o
+ON otp.order_id = o.id
+WHERE o.customer_id = 1255;
  ----------CASE
 
  ---1 syntax
